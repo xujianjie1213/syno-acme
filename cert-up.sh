c067977ab03d1adec22af7fadf4bb33aae8f5ea8
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2021-11-22 14:04:55 +0800

    Update cert-up.sh

--- cert-up.sh
+++ cert-up.sh
@@ -52,7 +52,7 @@ generateCrt () {
   source config
   echo 'begin updating default cert by acme.sh tool'
   source ${ACME_BIN_PATH}/acme.sh.env 
-  ${ACME_BIN_PATH}/acme.sh --register-account -m rylan.xujianjie@gmail.com --server zerossl
+  ${ACME_BIN_PATH}/acme.sh --register-account -m xxx@gmail.com --server zerossl
   ${ACME_BIN_PATH}/acme.sh --force --log --issue --dns ${DNS} --dnssleep ${DNS_SLEEP} -d "${DOMAIN}" -d "*.${DOMAIN}"
   ${ACME_BIN_PATH}/acme.sh --force --installcert -d ${DOMAIN} -d *.${DOMAIN} \
     --certpath ${CRT_PATH}/cert.pem \
